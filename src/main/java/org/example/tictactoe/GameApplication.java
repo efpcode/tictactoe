73deb6b85355645dc6db75ada88138f0cf3b06b9
efpcode <efpjobhunt@gmail.com>
2024-10-21 13:36:32 +0200

    Added Controllers for views
    Quick note created an exit method for Quit button.

--- src/main/java/org/example/tictactoe/GameApplication.java
+++ src/main/java/org/example/tictactoe/GameApplication.java
@@ -2,18 +2,30 @@ package org.example.tictactoe;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GameApplication extends Application {
+    final int width = 960;
+    final int height = 1440;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 960, 1440);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+
+        FXMLLoader gameMenu = new FXMLLoader(GameApplication.class.getResource("game-menu-view.fxml"));
+        Parent root = gameMenu.load();
+        Scene menuScene = new Scene(root, width, height);
+
+
+        FXMLLoader gameView = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
+        Parent view = gameView.load();
+        Scene gameScene = new Scene(view, width, height);
+
+
+        stage.setTitle("Welcome to TicTacToe!");
+        stage.setScene(menuScene);
         stage.show();
     }
 
