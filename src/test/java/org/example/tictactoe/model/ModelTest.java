17933c9e145b5128ba661925ca25fd4ce604aa60
efpcode <efpjobhunt@gmail.com>
2024-10-22 13:00:30 +0200

    Added Assertj
    new module

---
+++ src/test/java/org/example/tictactoe/model/ModelTest.java
@@ -0,0 +1,13 @@
+package org.example.tictactoe.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ModelTest {
+    @Test
+    void test(){
+        //assertThat()
+    }
+
+}
