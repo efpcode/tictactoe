ed4adbf662a60d18e93bf5c03a7b2b7a4ad15327
efpcode <efpjobhunt@gmail.com>
2024-10-29 13:44:28 +0100

    Fixed math bug
    Math bug for player.

--- src/main/java/org/example/tictactoe/model/Player.java
+++ src/main/java/org/example/tictactoe/model/Player.java
@@ -5,7 +5,10 @@ package org.example.tictactoe.model;
 public record Player(Matrix2dPositions position, PlayerToken token) {
     public int getLinearRepresentation(int steps){
         int step = position.row()>=1 ? steps-1 : 0;
-        return position().row() + step +position().column();
+        if (step == 0){
+            return position().row() + step + position().column();
+        }
+        return (int) Math.pow(step, position().row() ) + position().column();
 
     }
 
