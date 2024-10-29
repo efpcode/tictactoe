9e5c207f52d6b5364dc6e5a906e9b2059b2aa822
efpcode <efpjobhunt@gmail.com>
2024-10-29 06:06:36 +0100

    Remove bug
    Remove bug from getLinearRepresentation method

--- src/main/java/org/example/tictactoe/model/Player.java
+++ src/main/java/org/example/tictactoe/model/Player.java
@@ -4,9 +4,8 @@ package org.example.tictactoe.model;
 
 public record Player(Matrix2dPositions position, PlayerToken token) {
     public int getLinearRepresentation(int steps){
-        int factor = position.row()/steps;
-        int step = factor>=1 ? 0 : steps-1;
-        return factor + step +position().column();
+        int step = position.row()>=1 ? steps-1 : 0;
+        return position().row() + step +position().column();
 
     }
 
